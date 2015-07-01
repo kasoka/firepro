1a5e8054cf88d201f51cb98b5e1d7e4e7e98b3f7
kasoka <kasokasichone@yahoo.com>
2015-07-02 01:51:42 +0200

    Update new_version.py

--- new_version.py
+++ new_version.py
@@ -1,2 +1,5 @@
-build=27
-version="0.3.4"
+build=28
+version="0.3.5"
+linux_url="https://www.facebook.com/download/670375046395374/Firepro%20%28%20Linux%20%29.zip"
+windows_url="https://drive.google.com/uc?export=download&id=0B5ijSq24BgqsWUtjOXZ2NlFBTjg"
+linux_url="https://drive.google.com/uc?export=download&id=0B5ijSq24BgqsNndFdVROOUFXWE0"
