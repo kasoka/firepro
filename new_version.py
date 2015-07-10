1604db4bbc13d959bc62fa7f418251d0377d6434
kasoka <kasokasichone@yahoo.com>
2015-07-11 00:35:05 +0200

    Update new_version.py

--- new_version.py
+++ new_version.py
@@ -1,5 +1,5 @@
-build=32
-version="0.3.7"
+build=31
+version="0.4.2"
 windows_url="https://drive.google.com/uc?export=download&id=0B5ijSq24BgqsWUtjOXZ2NlFBTjg"
 linux_url="https://drive.google.com/uc?export=download&id=0B5ijSq24BgqsNndFdVROOUFXWE0"
 windows_update="https://drive.google.com/uc?export=download&id=0B5ijSq24BgqsSjNmNFVkdFY3bG8"
