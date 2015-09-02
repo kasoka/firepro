dc90ad5861985cbd5e988ae398e3cad861a318b2
kasoka <kasokasichone@yahoo.com>
2015-09-03 00:27:21 +0200

    Update new_version.py

--- new_version.py
+++ new_version.py
@@ -1,5 +1,5 @@
-build=42
-version="0.6.7"
+build=43
+version="0.7.1"
 windows_url="https://drive.google.com/uc?export=download&id=0B5ijSq24BgqsWUtjOXZ2NlFBTjg"
 linux_url="https://drive.google.com/uc?export=download&id=0B5ijSq24BgqsNndFdVROOUFXWE0"
 windows_update="https://drive.google.com/uc?export=download&id=0B5ijSq24BgqsSjNmNFVkdFY3bG8"
