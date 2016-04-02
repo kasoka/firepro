b2b3395cd91a2e2b9b88ba72b25f45495d6addb8
kasoka <kasokasichone@yahoo.com>
2016-04-03 00:09:04 +0200

    Update new_version.py

--- new_version.py
+++ new_version.py
@@ -1,4 +1,4 @@
-build=239
-version="6.0.5"
+build=245
+version="6.3.7"
 windows_url="https://drive.google.com/uc?export=download&id=0B5ijSq24BgqsWUtjOXZ2NlFBTjg"
 windows_update="https://drive.google.com/uc?export=download&id=0B5ijSq24BgqsSjNmNFVkdFY3bG8"
