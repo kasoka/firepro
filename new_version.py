030f8b9d85c158b7fe01ebd9009455493e74766a
kasoka <kasokasichone@yahoo.com>
2016-05-15 00:38:25 +0200

    Update new_version.py

--- new_version.py
+++ new_version.py
@@ -1,4 +1,4 @@
-build=268
-version="7.7.9"
+build=270
+version="7.9.0"
 windows_url="https://drive.google.com/uc?export=download&id=0B5ijSq24BgqsWUtjOXZ2NlFBTjg"
 windows_update="https://drive.google.com/uc?export=download&id=0B5ijSq24BgqsSjNmNFVkdFY3bG8"
