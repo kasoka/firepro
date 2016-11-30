ccb14808e3192c19682a7ec385b00adf94a1c497
kasoka <kasokasichone@yahoo.com>
2016-11-30 21:01:22 +0200

    Update new_version.py

--- new_version.py
+++ new_version.py
@@ -1,4 +1,4 @@
-build=284
-version="8.1.1"
+build=504
+version="13.1.8"
 windows_url="https://drive.google.com/uc?export=download&id=0B5ijSq24BgqsWUtjOXZ2NlFBTjg"
 windows_update="https://drive.google.com/uc?export=download&id=0B5ijSq24BgqsSjNmNFVkdFY3bG8"
